5dae6d0dbd5bbabc9202cdc2a3684d62ee7d51ec
unknown <thomaskuwaikwan@gmail.com>
2024-11-14 11:13:00 +0800

    implemented timer and json

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,53 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
+#include <FS.h>
+#include <SD.h>
+#include <SPI.h>
+
+//sensor
+#define lightPin 4
+
 int light;
+const int size_1mb = 1048576;     //1 mb size 
+unsigned long startTime;
+unsigned long currentTime;
+DynamicJsonDocument doc (2*size_1mb) ; //allocate heap memory(value) to create Json document
 
-// put function declarations here:
-int myFunction(int, int);
+/*
+operator
+doc[key] = value -> "key" : value
+*/
+int lightValue()
+{
+  light = analogRead(lightPin);
+  return light;
+}  
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup() 
+{
+  startTime = millis();
+  pinMode(lightPin, INPUT);
+  Serial.begin(9600); //testing
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  currentTime = millis();
+  if ((currentTime-startTime) >= 1000)
+  {
+    if (doc.memoryUsage() <= size_1mb)    
+    {
+      doc["Reading"]["Timestamp"] = currentTime;
+      Serial.println(currentTime); //testing
+      doc["Reading"]["light"] = lightValue();
+      Serial.println(light); //testing
+      startTime = currentTime; //start the new cycle
+    }
+    else //exceeded 1mb size
+    {
+      doc.clear();
+      Serial.println("Exceeded size"); //testing
+    }
+    
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
