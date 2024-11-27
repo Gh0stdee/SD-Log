7562d6271ef6fa644c70ff021139b77f990cb03e
unknown <thomaskuwaikwan@gmail.com>
2024-11-27 11:54:20 +0800

    Implemented formating and added formatting comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,11 @@
 //TimeStamp Fomat Min:Sec
 String getTimestamp()
 {
-  time_t timeInfo = time(NULL);
-  struct tm date =*localtime(&timeInfo);
-  String minute = (String)date.tm_min;
-  String second = (String)date.tm_sec;
-  String timeStamp = (minute +":"+ second);
+  time_t timeInfo = time(NULL);          //time_t for timestamps
+  struct tm date =*localtime(&timeInfo); //struct tm for datatime struct and access to members
+  char Timeformat[50];                   //char array/string for storing the formatted date
+  strftime(Timeformat, 50, "%H:%M:%S", &date);  //strfttime formatting
+  String timeStamp = Timeformat;
   return timeStamp;
 }
 
@@ -22,7 +22,7 @@ int lightValue()
   light = analogRead(lightPin);
   return light;
 }  
-DynamicJsonDocument doc (2*size_1mb) ;
+DynamicJsonDocument doc (2*1024) ; //size_1mb
 String dataMessage;
 
 //create file if file doesn't exist
@@ -31,7 +31,7 @@ void newFileCreate()
   File file = SD.open("/data.txt");                     //create a file on SD card and open the file
   if(!file)
   {
-    writeFile(SD,"/data.txt","Timestamp, Light \r\n"); // \r\n means go to next line in the file
+    writeFile(SD,"/lightData.txt","Timestamp Light \r\n"); // \r\n means go to next line in the file
     Serial.println("Creating file...");
     file.close();                                      //close the file
   }
@@ -48,9 +48,8 @@ void setup()
     Serial.println("Error occurred at SD begin");
     delay(1000);
   }
+
   //SD
-  
-  
   newFileCreate();
   
   //sensor and timer
@@ -68,17 +67,17 @@ void loop() {
   currentTime = millis();
   if ((currentTime-startTime) >= 1000)
   { 
-    if (doc.memoryUsage() <= size_1mb)    
+    if (doc.memoryUsage() <= 2048)    //size_1mb
     {
-      Serial.println("check 2 successful");
+      Serial.print(doc.memoryUsage());
       doc["Reading"]["Timestamp"] = getTimestamp();
       //testing
-      Serial.println(currentTime); 
+      Serial.println(getTimestamp()); 
       doc["Reading"]["light"] = lightValue();
       //testing
       Serial.println(light); 
-      dataMessage = String(currentTime) + "," + String(light) +"\r\n";
-      appendFile(SD,"/data.txt",dataMessage.c_str());
+      dataMessage = getTimestamp() + "  " + String(light) +"\r\n";
+      appendFile(SD,"/lightData.txt",dataMessage.c_str());
       //testing
       //Serial.print("Data saved(TimeStamp, Light): ");
       //Serial.println(currentTime, light);
@@ -90,10 +89,8 @@ void loop() {
     {
       //testing
       Serial.println("Exceeded predetermined size"); 
-      
-      
       doc.clear();
-      deleteFile(SD, "/data.txt");
+      deleteFile(SD, "/lightData.txt");
       newFileCreate();
       startTime = currentTime; //start the new cycle
       
